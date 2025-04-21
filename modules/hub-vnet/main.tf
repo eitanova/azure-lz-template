45c6275d20ef5a2adc22d4f97bd1717cefa13af0
Philip Levin <philiplevin20@gmail.com>
2025-04-21 15:31:05 +0300

    created a hub vnet module

---
+++ modules/hub-vnet/main.tf
@@ -0,0 +1,49 @@
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+  tags     = var.resource_group_tags
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = var.virtual_network_name
+  address_space       = [var.virtual_network_address_space]
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+}
+
+resource "azurerm_virtual_network_dns_servers" "this" {
+  virtual_network_id = azurerm_virtual_network.this.id
+  dns_servers        = var.dns_servers
+}
+
+resource "azurerm_subnet" "this" {
+  for_each             = var.subnets
+  name                 = each.key
+  address_prefixes     = [each.value]
+  virtual_network_name = azurerm_virtual_network.this.name
+  resource_group_name  = azurerm_resource_group.this.name
+}
+
+
+resource "azurerm_route_table" "this" {
+  name                = var.route_table.route_table_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  route {
+    name                   = var.route_table.route_name
+    address_prefix         = var.route_table.route_destination_prefix
+    next_hop_type          = var.route_table.route_next_hop_type
+    next_hop_in_ip_address = var.route_table.route_next_hop_ip
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "this" {
+  for_each = {
+    for name, cidr in var.subnets : name => cidr
+    if name != "AzureFirewallSubnet" && name != "GatewaySubnet" && name != "PaloAltoSubnet"
+  }
+
+  subnet_id      = azurerm_subnet.this[each.key].id
+  route_table_id = azurerm_route_table.this.id
+}
---
+++ modules/hub-vnet/variables.tf
@@ -0,0 +1,56 @@
+variable "resource_group_name" {
+  description = "The name of the resource group where the virtual network and related resources will be created."
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "Azure region where the resource group and all resources will be deployed."
+  type        = string
+}
+
+variable "resource_group_tags" {
+  description = "A map of tags to apply to the resource group and its resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "virtual_network_name" {
+  description = "The name of the Azure Virtual Network to be created."
+  type        = string
+}
+
+variable "virtual_network_address_space" {
+  description = "The address space (CIDR block) of the virtual network."
+  type        = string
+}
+
+variable "dns_servers" {
+  description = "A list of custom DNS server IP addresses for the virtual network. Leave empty to use Azure's default."
+  type        = list(string)
+}
+
+variable "subnets" {
+  description = <<EOT
+A map of subnets to create in the virtual network.
+Key = subnet name
+Value = address prefix (CIDR block)
+
+Example:
+{
+  "AppSubnet"         = "10.0.1.0/24",
+  "AzureFirewallSubnet" = "10.0.2.0/24"
+}
+EOT
+
+  type = map(string)
+}
+
+variable "route_table" {
+  type = object({
+    name               = string
+    route_name         = string
+    destination_prefix = string
+    next_hop_type      = string
+    next_hop_ip        = string
+  })
+}
