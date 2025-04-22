626866d1964b661d954669d69ad0180b5bc64d02
Philip <72705461+DeOriginal@users.noreply.github.com>
2025-04-22 17:25:02 +0300

    added sample division yaml

--- 3-divisions-factory/locals.tf
+++ 3-divisions-factory/locals.tf
@@ -1,7 +1,16 @@
-# To do = yamldecode actual hub values
 locals {
-  trusted_prod_hubs   = {}
-  untrusted_prod_hubs = {}
-  trusted_ppd_hubs    = {}
-  untrusted_ppd_hubs  = {}
+  division_config = yamldecode(file("${path.root}/config-dev/divisions/${var.division_name}/config.yml"))
+  
+  trusted_prod_hubs = {
+    (var.division_name) = local.division_config.hubs["trusted-prod"]
+  }
+  untrusted_prod_hubs = {
+    (var.division_name) = local.division_config.hubs["untrusted-prod"]
+  }
+  trusted_ppd_hubs = {
+    (var.division_name) = local.division_config.hubs["trusted-ppd"]
+  }
+  untrusted_ppd_hubs = {
+    (var.division_name) = local.division_config.hubs["untrusted-ppd"]
+  }
 }
