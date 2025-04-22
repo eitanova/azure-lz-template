96de604b291dd29ffadc2c94151b34ae3db2ce0b
Philip <72705461+DeOriginal@users.noreply.github.com>
2025-04-22 12:27:51 +0300

    changed from variable approach to yaml decode

--- 2-global-shared-services/main.tf
+++ 2-global-shared-services/main.tf
@@ -1,65 +1,112 @@
-module "hub-internet-prd" {
+locals {
+  dev_hubs  = yamldecode(file("${path.root}/config-dev/networking/hubs/development.yml"))
+  mgmt_hubs = yamldecode(file("${path.root}/config-dev/networking/hubs/management.yml"))
+  prod_hubs = yamldecode(file("${path.root}/config-dev/networking/hubs/production.yml"))
+
+  global_hubs = merge(
+    local.dev_hubs.hubs,
+    local.mgmt_hubs.hubs,
+    local.prod_hubs.hubs
+  )
+}
+
+
+module "internet-prd-hub" {
   source = "../modules/hub-vnet"
 
-  resource_group_name           = var.hubs["internet-prd"].resource_group_name
-  resource_group_location       = var.hubs["internet-prd"].resource_group_location
-  resource_group_tags           = var.hubs["internet-prd"].resource_group_tags
-  virtual_network_name          = var.hubs["internet-prd"].virtual_network_name
-  virtual_network_address_space = var.hubs["internet-prd"].virtual_network_address_space
-  dns_servers                   = var.hubs["internet-prd"].dns_servers
-  subnets                       = var.hubs["internet-prd"].subnets
-  route_table                   = var.hubs["internet-prd"].route_table
+  resource_group_name           = local.global_hubs["internet-prd"].resource_group_name
+  resource_group_location       = local.global_hubs["internet-prd"].resource_group_location
+  resource_group_tags           = local.global_hubs["internet-prd"].resource_group_tags
+  virtual_network_name          = local.global_hubs["internet-prd"].virtual_network_name
+  virtual_network_address_space = local.global_hubs["internet-prd"].virtual_network_address_space
+  dns_servers                   = local.global_hubs["internet-prd"].dns_servers
+  subnets                       = local.global_hubs["internet-prd"].subnets
+  route_table                   = local.global_hubs["internet-prd"].route_table
 
   providers = {
     azurerm = azurerm.internet-prd
   }
 }
 
-module "hub-internet-dev" {
+module "internet-dev-hub" {
   source = "../modules/hub-vnet"
 
-  resource_group_name           = var.hubs["internet-dev"].resource_group_name
-  resource_group_location       = var.hubs["internet-dev"].resource_group_location
-  resource_group_tags           = var.hubs["internet-dev"].resource_group_tags
-  virtual_network_name          = var.hubs["internet-dev"].virtual_network_name
-  virtual_network_address_space = var.hubs["internet-dev"].virtual_network_address_space
-  dns_servers                   = var.hubs["internet-dev"].dns_servers
-  subnets                       = var.hubs["internet-dev"].subnets
-  route_table                   = var.hubs["internet-dev"].route_table
+  resource_group_name           = local.global_hubs["internet-dev"].resource_group_name
+  resource_group_location       = local.global_hubs["internet-dev"].resource_group_location
+  resource_group_tags           = local.global_hubs["internet-dev"].resource_group_tags
+  virtual_network_name          = local.global_hubs["internet-dev"].virtual_network_name
+  virtual_network_address_space = local.global_hubs["internet-dev"].virtual_network_address_space
+  dns_servers                   = local.global_hubs["internet-dev"].dns_servers
+  subnets                       = local.global_hubs["internet-dev"].subnets
+  route_table                   = local.global_hubs["internet-dev"].route_table
 
   providers = {
     azurerm = azurerm.internet-dev
   }
 }
 
-module "hub-mgmt" {
+module "trusted-dev-hub" {
   source = "../modules/hub-vnet"
 
-  resource_group_name           = var.hubs["mgmt"].resource_group_name
-  resource_group_location       = var.hubs["mgmt"].resource_group_location
-  resource_group_tags           = var.hubs["mgmt"].resource_group_tags
-  virtual_network_name          = var.hubs["mgmt"].virtual_network_name
-  virtual_network_address_space = var.hubs["mgmt"].virtual_network_address_space
-  dns_servers                   = var.hubs["mgmt"].dns_servers
-  subnets                       = var.hubs["mgmt"].subnets
-  route_table                   = var.hubs["mgmt"].route_table
+  resource_group_name           = local.global_hubs["internet-dev"].resource_group_name
+  resource_group_location       = local.global_hubs["internet-dev"].resource_group_location
+  resource_group_tags           = local.global_hubs["internet-dev"].resource_group_tags
+  virtual_network_name          = local.global_hubs["internet-dev"].virtual_network_name
+  virtual_network_address_space = local.global_hubs["internet-dev"].virtual_network_address_space
+  dns_servers                   = local.global_hubs["internet-dev"].dns_servers
+  subnets                       = local.global_hubs["internet-dev"].subnets
+  route_table                   = local.global_hubs["internet-dev"].route_table
+
+  providers = {
+    azurerm = azurerm.internet-dev
+  }
+}
+
+module "untrusted-dev-hub" {
+  source = "../modules/hub-vnet"
+
+  resource_group_name           = local.global_hubs["untrusted-dev"].resource_group_name
+  resource_group_location       = local.global_hubs["untrusted-dev"].resource_group_location
+  resource_group_tags           = local.global_hubs["untrusted-dev"].resource_group_tags
+  virtual_network_name          = local.global_hubs["untrusted-dev"].virtual_network_name
+  virtual_network_address_space = local.global_hubs["untrusted-dev"].virtual_network_address_space
+  dns_servers                   = local.global_hubs["untrusted-dev"].dns_servers
+  subnets                       = local.global_hubs["untrusted-dev"].subnets
+  route_table                   = local.global_hubs["untrusted-dev"].route_table
+
+  providers = {
+    azurerm = azurerm.untrusted-dev
+  }
+}
+
+module "mgmt-hub" {
+  source = "../modules/hub-vnet"
+
+  resource_group_name           = local.global_hubs["mgmt"].resource_group_name
+  resource_group_location       = local.global_hubs["mgmt"].resource_group_location
+  resource_group_tags           = local.global_hubs["mgmt"].resource_group_tags
+  virtual_network_name          = local.global_hubs["mgmt"].virtual_network_name
+  virtual_network_address_space = local.global_hubs["mgmt"].virtual_network_address_space
+  dns_servers                   = local.global_hubs["mgmt"].dns_servers
+  subnets                       = local.global_hubs["mgmt"].subnets
+  route_table                   = local.global_hubs["mgmt"].route_table
 
   providers = {
     azurerm = azurerm.mgmt
   }
 }
 
-module "hub-on-prem" {
+module "on-prem-hub" {
   source = "../modules/hub-vnet"
 
-  resource_group_name           = var.hubs["on-prem"].resource_group_name
-  resource_group_location       = var.hubs["on-prem"].resource_group_location
-  resource_group_tags           = var.hubs["on-prem"].resource_group_tags
-  virtual_network_name          = var.hubs["on-prem"].virtual_network_name
-  virtual_network_address_space = var.hubs["on-prem"].virtual_network_address_space
-  dns_servers                   = var.hubs["on-prem"].dns_servers
-  subnets                       = var.hubs["on-prem"].subnets
-  route_table                   = var.hubs["on-prem"].route_table
+  resource_group_name           = local.global_hubs["on-prem"].resource_group_name
+  resource_group_location       = local.global_hubs["on-prem"].resource_group_location
+  resource_group_tags           = local.global_hubs["on-prem"].resource_group_tags
+  virtual_network_name          = local.global_hubs["on-prem"].virtual_network_name
+  virtual_network_address_space = local.global_hubs["on-prem"].virtual_network_address_space
+  dns_servers                   = local.global_hubs["on-prem"].dns_servers
+  subnets                       = local.global_hubs["on-prem"].subnets
+  route_table                   = local.global_hubs["on-prem"].route_table
 
   providers = {
     azurerm = azurerm.on-prem
