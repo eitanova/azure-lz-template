0f901e0df32e2bb6f188a0a617c6411f9a258e43
Philip <72705461+DeOriginal@users.noreply.github.com>
2025-04-23 19:58:51 +0300

    converted yaml maps to hcl maps

--- 2-global-shared-services/locals.tf
+++ 2-global-shared-services/locals.tf
@@ -3,9 +3,43 @@ locals {
   mgmt_hubs = yamldecode(file("${var.paths.config}/networking/hubs/management.yml"))
   prod_hubs = yamldecode(file("${var.paths.config}/networking/hubs/production.yml"))
 
+  # Transform the subnet configuration for each hub
+  transformed_dev_hubs = {
+    for hub_name, hub_config in local.dev_hubs.hubs : hub_name => merge(
+      hub_config,
+      {
+        subnets = {
+          for subnet_name, subnet_config in hub_config.subnets : subnet_name => subnet_config.address_prefixes[0]
+        }
+      }
+    )
+  }
+
+  transformed_mgmt_hubs = {
+    for hub_name, hub_config in local.mgmt_hubs.hubs : hub_name => merge(
+      hub_config,
+      {
+        subnets = {
+          for subnet_name, subnet_config in hub_config.subnets : subnet_name => subnet_config.address_prefixes[0]
+        }
+      }
+    )
+  }
+
+  transformed_prod_hubs = {
+    for hub_name, hub_config in local.prod_hubs.hubs : hub_name => merge(
+      hub_config,
+      {
+        subnets = {
+          for subnet_name, subnet_config in hub_config.subnets : subnet_name => subnet_config.address_prefixes[0]
+        }
+      }
+    )
+  }
+
   global_hubs = merge(
-    local.dev_hubs.hubs,
-    local.mgmt_hubs.hubs,
-    local.prod_hubs.hubs
+    local.transformed_dev_hubs,
+    local.transformed_mgmt_hubs,
+    local.transformed_prod_hubs
   )
-} 
+}
