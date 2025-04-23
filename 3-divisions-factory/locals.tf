a23c90d471e7d052508ee9edd7808a9c0d82f1dd
Philip <72705461+DeOriginal@users.noreply.github.com>
2025-04-23 22:26:37 +0300

    fixed the subnet  data fetched from yaml

--- 3-divisions-factory/locals.tf
+++ 3-divisions-factory/locals.tf
@@ -3,8 +3,24 @@ locals {
   division_config = yamldecode(file("${var.paths.config}/divisions/${var.division_name}/config.yml"))
 
   # Map the configuration to the appropriate local variables
-  trusted_prod_hub   = local.division_config.environments.prd.shared_services.networking.trusted
-  untrusted_prod_hub = local.division_config.environments.prd.shared_services.networking.untrusted
-  trusted_ppd_hub    = local.division_config.environments.ppd.shared_services.networking.trusted
-  untrusted_ppd_hub  = local.division_config.environments.ppd.shared_services.networking.untrusted
+  trusted_prod_hub = merge(local.division_config.environments.prd.shared_services.networking.trusted, {
+    subnets = {
+      palo = local.division_config.environments.prd.shared_services.networking.trusted.subnets.palo.address_prefixes[0]
+    }
+  })
+  untrusted_prod_hub = merge(local.division_config.environments.prd.shared_services.networking.untrusted, {
+    subnets = {
+      palo = local.division_config.environments.prd.shared_services.networking.untrusted.subnets.palo.address_prefixes[0]
+    }
+  })
+  trusted_ppd_hub = merge(local.division_config.environments.ppd.shared_services.networking.trusted, {
+    subnets = {
+      palo = local.division_config.environments.ppd.shared_services.networking.trusted.subnets.palo.address_prefixes[0]
+    }
+  })
+  untrusted_ppd_hub = merge(local.division_config.environments.ppd.shared_services.networking.untrusted, {
+    subnets = {
+      palo = local.division_config.environments.ppd.shared_services.networking.untrusted.subnets.palo.address_prefixes[0]
+    }
+  })
 }
