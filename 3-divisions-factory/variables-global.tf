ddb298dcbf2e32fbf268b806fd5c61b096b7639a
Philip <72705461+DeOriginal@users.noreply.github.com>
2025-04-23 22:27:53 +0300

    moved non division speciific vars to global

--- 3-divisions-factory/variables-global.tf
+++ 3-divisions-factory/variables-global.tf
@@ -17,6 +17,44 @@ variable "tenant" {
     prefix             = optional(string, null)
     billing_name       = string
     enrollment_account = string
-    locations          = string
+    location           = string
   })
 }
+
+variable "subscriptions" {
+  type = map(object({
+    id           = string
+    display_name = string
+  }))
+}
+
+variable "prod_internet_vnet_id" {
+  type        = string
+  description = "The id of the internet hub vnet for the prd spoke"
+}
+
+variable "prod_internet_vnet_name" {
+  type        = string
+  description = "The name of the internet hub vnet for the prd spoke"
+}
+
+variable "prod_internet_vnet_rg" {
+  type        = string
+  description = "The rg of the internet hub vnet for the prd spoke"
+}
+
+variable "on_prem_vnet_id" {
+  type        = string
+  description = "The id of the on prem hub vnet"
+}
+
+variable "on_prem_vnet_name" {
+  type        = string
+  description = "The name of the internet hub vnet for the dev spoke"
+}
+
+variable "on_prem_vnet_rg" {
+  type        = string
+  description = "The rg of the internet hub vnet for the dev spoke"
+}
+
--- 3-divisions-factory/variables.tf
+++ 3-divisions-factory/variables.tf
@@ -2,33 +2,3 @@ variable "division_name" {
   type        = string
   description = "The name of the division to create the Hub for"
 }
-
-variable "prod_internet_vnet_id" {
-  type        = string
-  description = "The id of the internet hub vnet for the prd spoke"
-}
-
-variable "prod_internet_vnet_name" {
-  type        = string
-  description = "The name of the internet hub vnet for the prd spoke"
-}
-
-variable "prod_internet_vnet_rg" {
-  type        = string
-  description = "The rg of the internet hub vnet for the prd spoke"
-}
-
-variable "on_prem_vnet_id" {
-  type        = string
-  description = "The id of the on prem hub vnet"
-}
-
-variable "on_prem_vnet_name" {
-  type        = string
-  description = "The name of the internet hub vnet for the dev spoke"
-}
-
-variable "on_prem_vnet_rg" {
-  type        = string
-  description = "The rg of the internet hub vnet for the dev spoke"
-}
