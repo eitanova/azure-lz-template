6b241d1f4a259b9fc33edb853a4cb6ff42c07ca1
Philip <72705461+DeOriginal@users.noreply.github.com>
2025-04-23 12:23:40 +0300

    reflected the division yaml change on the local vars

--- 3-divisions-factory/locals.tf
+++ 3-divisions-factory/locals.tf
@@ -3,8 +3,8 @@ locals {
   division_config = yamldecode(file("${path.root}/config-dev/divisions/${var.division_name}/config.yml"))
 
   # Map the configuration to the appropriate local variables
-  trusted_prod_hub   = local.division_config.hubs["trusted-prd"]
-  untrusted_prod_hub = local.division_config.hubs["untrusted-prd"]
-  trusted_ppd_hub    = local.division_config.hubs["trusted-ppd"]
-  untrusted_ppd_hub  = local.division_config.hubs["untrusted-ppd"]
+  trusted_prod_hub   = local.division_config.environments.prd.shared_services.networking.trusted
+  untrusted_prod_hub = local.division_config.environments.prd.shared_services.networking.untrusted
+  trusted_ppd_hub    = local.division_config.environments.ppd.shared_services.networking.trusted
+  untrusted_ppd_hub  = local.division_config.environments.ppd.shared_services.networking.untrusted
 }
